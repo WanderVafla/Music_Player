90444bd77f70a493924f7877363a844847d50db3
WanderVafla <procenkovladik1@gmail.com>
2025-10-21 21:49:57 +0200

    new commit

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,118 @@
-use std::{io::sink, option};
-use std::fs::File;
-use std::io::BufReader;
-use rodio::{Decoder, OutputStreamBuilder, Sink};
+use std::{fs::{self, File}, io::{BufReader, Write}, path::PathBuf, time::Duration};
 
-use eframe::egui;
+use eframe::{egui::{self, Button, CentralPanel, ColorImage, CornerRadius, TextureHandle, TextureOptions}, epaint::tessellator::Path, glow::{MAX_DUAL_SOURCE_DRAW_BUFFERS, PRIMITIVES_SUBMITTED}};
 
-// mod charge_songs;
+use lofty::{file::{AudioFile, TaggedFileExt}, probe, read_from_path, tag::ItemKey};
+use rodio::{Decoder, OutputStream, OutputStreamBuilder, Sink};
+use serde::de::value::Error;
+
+use clap::{builder::Str, Parser};
+
+use serde::{Deserialize, Serialize};
+
+use serde_json::from_str;
+
+use symphonia::core::audio::{AudioBufferRef, Signal};
+use symphonia::core::codecs::DecoderOptions;
+use symphonia::core::formats::FormatOptions;
+use symphonia::core::io::MediaSourceStream;
+use symphonia::default::{get_codecs, get_probe};
 
 fn main() -> Result<(), eframe::Error> {
-    let option = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_resizable(true)
-            .with_inner_size([800.0, 500.0]),
-        ..Default::default()
-    };
+    let mut options = eframe::NativeOptions::default(); // создаём по умолчанию
+
+    // Настраиваем размер окна через viewport
+    options.viewport = egui::ViewportBuilder::default()
+        .with_inner_size([800.0, 600.0]);
     eframe::run_native(
         "Моё первое окно",
-        option,
-        Box::new(|_cc| Ok(Box::new(MyApp::new())))
+        options,
+        Box::new(|_cc| Ok(Box::new(MyApp::new()))),
     )
 }
-
+#[derive(Deserialize, Serialize, Debug)]
+struct Song_Data {
+    title: String,
+    artist: String,
+    durration: Duration,
+    path: PathBuf,
+}
 struct MyApp {
     stream: rodio::OutputStream,
-    sink: Sink,
-    volume: f32
-}
+    sink: rodio::Sink,
 
+    current_index: usize,
+    playlist: Vec<Song_Data>,
+    initialized: bool,
 
+    current_durration: Duration,
+    playing: bool,
 
+    loopped: bool,
+    random: bool,
+    volume: f32,
+}   
 
 impl MyApp {
     fn new() -> Self {
         let stream = OutputStreamBuilder::open_default_stream().unwrap();
-        let sink = Sink::connect_new(stream.mixer());
-        let volume = 50.0;
-        Self { stream, sink, volume }
+        let sink = rodio::Sink::connect_new(stream.mixer());
+
+        let current_index = 0;
+        let playlist = vec![];
+        let initialized: bool = false;
+
+        let current_durration = sink.get_pos();
+        let playing = false;
+
+        let loopped = false;
+        let random = false;
+        let volume = 1.0;
+
+
+        Self { stream, sink, current_index, playlist, initialized, current_durration, playing, loopped, random, volume }
+   }
+
+    fn load_song_queue(&mut self) {
+        let file = PathBuf::from(r"C:\Users\User\Music\music");
+        
+        for song in fs::read_dir(file).unwrap() {
+            let song_entry = song.unwrap();
+            let path = song_entry.path();
+
+            let tagged_file_result = read_from_path(&path);
+            if let Ok(tagged_file) = tagged_file_result {
+                if let Some(tag) = tagged_file.primary_tag() {
+                    let title: String = tag.get_string(&ItemKey::TrackTitle).unwrap_or("Unknow Title").to_string();                        
+                    // let album: String = tag.get_string(&ItemKey::AlbumTitle).unwrap_or("Unknow Title").to_string();                         
+                    let artist: String = tag.get_string(&ItemKey::TrackArtist).unwrap_or("Unknow Title").to_string();
+
+                    let durration = tagged_file.properties().duration();
+
+            let song_data = Song_Data {
+                title,
+                artist,
+                durration,
+                path: path.clone()
+            };
+            self.playlist.push(song_data);
+            }
+        }
+    }
+   
     }
 
-    fn play_music(&self) {
-        let path = r"C:\Users\User\Music\music\Gruppa Skryptonite - Снегопад.mp3";
-
-        let file = File::open(path).expect("Не удалось открыть файл");
-        let source = Decoder::new(BufReader::new(file)).expect("Не удалось декодировать");
+    fn play_current(&mut self) {
+        let path = &self.playlist[self.current_index].path;
+        let file = File::open(path).expect("Errore to open music file");
+    
+        let source = Decoder::new(BufReader::new(file)).expect("Errore with a decoder");
 
         self.sink.stop();
-        println!("song is stoped");
         self.sink.append(source);
-        println!("song is appended");
         self.sink.play();
-        println!("song is playing");
+
+        self.playing = true;
     }
 
     fn playback_music(&self) {
@@ -60,65 +123,135 @@ impl MyApp {
         }
     }
 
-    fn back_music(&self) {
-        println!("back")
+    fn prev_song(&mut self) {
+        if self.current_index == 0 {
+            self.play_current();
+            println!("Current index: {}", self.current_index);
+        } else {
+            self.current_index = self.current_index - 1;
+            self.play_current();
+            println!("Current index: {}", self.current_index);
+        } 
+        println!("prev!");
     }
-
-    fn next_music(&self) {
-        self.sink.skip_one();
-        println!("next")
+    fn next_song(&mut self) {
+        if self.current_index == self.playlist.len() {
+            self.play_current();
+            println!("Current index: {}", self.current_index);
+        } else {
+            self.current_index = self.current_index + 1;
+            self.play_current();
+            println!("Current index: {}", self.current_index);
+        }
+        println!("next!");
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        egui::SidePanel::left("song_list")
-        .resizable(false)
+        if self.initialized == false {
+            self.load_song_queue();
+            self.initialized = true;
+        } 
+        let mut clicked_index: Option<usize> = None;
+
+        egui::SidePanel::left("Playlist").resizable(false)
         .min_width(250.0)
         .default_width(250.0)
         .show(ctx,|ui| {
-            ui.horizontal(|ui| {
-                if ui.button("button").clicked() {
-                    self.play_music();
+
+            if ui.add_sized([10.0, 10.0], egui::Button::new("")).clicked() {
+                println!("PORNO!!!!")
+            }
+
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                for (index, song_data_item) in self.playlist.iter_mut().enumerate() {
+                    ui.horizontal(|ui| { 
+                        if ui.add_sized([64.0, 64.0], egui::Button::new("song").corner_radius(10)).clicked() {
+                            clicked_index = Some(index);
+                        }
+                        ui.vertical(|ui| {
+                            ui.label(&song_data_item.title);
+                            ui.label(&song_data_item.artist);
+                        });
+                    });
                 }
-    
-                ui.vertical(|ui| {
-                    ui.label("title");
-                    ui.label("artist");
-                });
             });
         });
 
-        
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Heading");
-            egui::TopBottomPanel::bottom("playback").show(ctx, |ui| {
-                
-                
-                ui.horizontal_centered(|ui| {
-                    ui.horizontal(|ui| {
-                        if ui.button("back").clicked() {
-                            self.back_music();
+        if let Some(index) = clicked_index {
+            self.current_index = index;
+            println!("current song: {}", self.current_index);
+            self.play_current();
+            }
+
+        egui::CentralPanel::default().show(ctx, |ui|{
+            ui.vertical(|ui| {
+                ui.label(self.playlist[self.current_index].title.clone());
+                ui.label(self.playlist[self.current_index].artist.clone());
+            });
+
+            ui.horizontal(|ui| {
+                let max_durration = self.playlist[self.current_index].durration.as_secs_f32();
+                self.current_durration = self.sink.get_pos();
+
+                ui.horizontal(|ui| {
+                    ui.label(self.current_durration.as_secs().to_string());
+                    if ui.add(egui::Slider::new(&mut self.current_durration.as_secs_f32(),
+                    0.0..=max_durration).show_value(false)).changed() {
+                        println!("durration: {}", self.current_durration.as_secs());
+                    }
+                    ui.label(max_durration.to_string());
+                });
+                if self.playing == true {
+                    if self.sink.empty() == true {
+                        if self.loopped == true {
+                            self.play_current();
+                        } else {
+                            self.current_index = self.current_index + 1;
+                            self.play_current();
                         }
-                        
+                        println!("song is finished");
+                    }
+                }
+            });
+
+            egui::TopBottomPanel::bottom("PlaybackPanel").show(ctx, |ui| {
+                ui.horizontal(|ui| {
+                    if ui.add_sized([16.0, 16.0], egui::Checkbox::new(&mut self.loopped, "")).changed() {
+                        println!("loop: {}", self.loopped);
+                    }
+
+                    ui.horizontal(|ui| {
+                        if ui.button("prev").clicked() {
+                            self.prev_song();
+                        }
                         if ui.button("play").clicked() {
-                            println!("play or pause clicked");
                             self.playback_music();
                         }
-                        
                         if ui.button("next").clicked() {
-                            self.next_music();
+                            self.next_song();
                         }
+
+                    if ui.add_sized([16.0, 16.0], egui::Checkbox::new(&mut self.random, ""))
+                    .changed() {
+                        println!("random: {}", self.random);
+                        }
+
                     });
-
-                    ui.add(egui::Slider::new(&mut self.volume, 0.0..=100.0)
+                    if ui.add(egui::Slider::new(&mut self.volume, 0.0..=10.0)
                         .vertical()
-                        .show_value(false)
-                    );
-
+                        .show_value(false))
+                        .changed() {
+                            self.sink.set_volume(self.volume);
+                            println!("volume: {}", self.volume);
+                        }
                 });
-            });
+            
         });
+    });
+
+       
     }
 }
 
